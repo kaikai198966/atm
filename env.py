2838f3aa2c6bc58aceedab98ea3b7cbb114e4cde
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-11-13 15:02:21 +0800

    create environment variables

---
+++ env.py
@@ -0,0 +1,7 @@
+bank_name = "ABC BANK"
+files = {
+    "users": "./atm-files/users.txt",
+    "accounts": "./atm-files/accounts.txt",
+    "transaction_details": "./atm-files/tran_details.txt",
+    "transaction_table": "./atm-files/tran_table.txt",
+}
