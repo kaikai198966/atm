dae31aa61f800e770f86bf4a84ebd4607b11bb78
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-11-13 15:05:25 +0800

    add admin account to env

--- env.py
+++ env.py
@@ -5,3 +5,9 @@ files = {
     "transaction_details": "./atm-files/tran_details.txt",
     "transaction_table": "./atm-files/tran_table.txt",
 }
+master_account = {
+    "username": "python@gmail.com",
+    "password": "admin",
+    "status": "adm",
+    "last_transaction_date": "10/31/2023_08:36:09",
+}
