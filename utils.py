bcb3e75e8449821944ceaed152ec96ff2d1dd920
mrwnmncd <69033265+mrwnmncd@users.noreply.github.com>
2023-11-13 19:02:45 +0800

    variable error

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ import time
 
 timestamp = lambda: time.strftime("%Y/%m/%d_%H:%M:%S")
 
-clear_console: lambda: os.system("cls" if os.name == "nt" else "clear") # NOTE: may be omitted, also omit line 1
+clear_console = lambda: os.system("cls" if os.name == "nt" else "clear") # NOTE: may be omitted, also omit line 1
